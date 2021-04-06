9597028ec27f7f2076a5c016aa06b7c00c2ff4a6
Santiago Mattiauda <santimattius@gmail.com>
2021-04-06 20:33:15 -0300

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Santiago Mattiauda on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
